c8d0d04cf201193292528c7efcd09a28356e1bff
Cristhian Gomez <crgomez@assanet.com>
2026-08-09 20:34:35 -0600

    chore: bootstrap spec-driven project governance

---
+++ src/test/java/ecommerce/tizo_api/TizoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.tizo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TizoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
